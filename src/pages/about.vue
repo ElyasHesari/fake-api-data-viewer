161624df53720c7632011e1bbcd9098c4f9df666
elyas_h <elyas_h@kuber.local>
2024-06-13 20:41:01 +0330

    test fetch api

---
+++ src/pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>about page</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const events = ref(null)
+console.log('test')
+onMounted(() => {
+    axios.get(
+        'https://my-json-server.typicode.com/code-pop/real-world_vue-3/events'
+    )
+    .then((response) => {
+        events.value = response.data
+        console.log('events: ', response.data)
+    })
+    .catch((error) => {
+        console.log('error:', error )
+    })
+})
+
+
+
+</script>
+
+<style>
+
+</style>
